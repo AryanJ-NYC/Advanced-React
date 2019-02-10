1c27fa8c12018755ccaadd795874f3bade4bdc80
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2019-02-09 22:20:55 -0500

    Finish item 23

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -7,6 +7,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   item: forwardTo('db'),
   items: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
   // below is commented out because the line above is the same thing (for learnsies!)
   // items(parent, args, ctx, info) {
   //   return ctx.db.query.items();
