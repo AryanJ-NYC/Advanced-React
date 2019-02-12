981312936a7a2c120df3717455a16efea481f3d4
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2019-02-11 20:54:20 -0500

    Finish item 26

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config({ path: 'variables.env' });
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const createServer = require('./createServer');
 const db = require('./db');
 
@@ -7,7 +8,15 @@ const server = createServer();
 
 // Use express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
-// TODO: Use express middleware to populate current user
+// decode JWT so we have userId on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -8,6 +8,13 @@ const Query = {
   item: forwardTo('db'),
   items: forwardTo('db'),
   itemsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    const { userId: id } = ctx.request;
+    if (!id) {
+      return;
+    }
+    return ctx.db.query.user({ where: { id } }, info);
+  },
   // below is commented out because the line above is the same thing (for learnsies!)
   // items(parent, args, ctx, info) {
   //   return ctx.db.query.items();
